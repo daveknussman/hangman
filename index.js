4336c979c5d047478b3b0261e4cca01c24c63ec3
daveknussman <dknussman@yahoo.com>
2019-10-23 19:30:09 -0400

    initial files for project

---
+++ index.js
@@ -0,0 +1,11 @@
+const prompt = require("readline-sync");
+const wordBank = require("./word-bank.json");
+
+console.log(wordBank);
+console.log(wordBank[7]);
+const myVariable = prompt.question("Please guess a letter: ");
+console.log(myVariable);
+console.log(myVariable);
+console.log(myVariable);
+console.log(myVariable + "\n\n");
+console.log(myVariable);
